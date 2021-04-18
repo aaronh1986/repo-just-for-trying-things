e347f6e8ea92c6558258e2fae0a53104e7a66d59
aaronh1986 <aaronharbourne@gmail.com>
2021-04-18 23:41:23 +0000

    Get close enough, for now.....

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,12 +11,24 @@ function calculateAnswer() {
     submit.addEventListener('click', function() {
 if (answer.value == numberOne + numberTwo) {
     alert('I guess you\'re not a fucking idiot after all.')
-    
+    resetGame();
 } else {
     alert('NOOOOOOOOOOOOOO, WROOOOOOOOOOOOOOOONG')
-   
+   resetGame();
 }
 })
 }
 
 calculateAnswer();
+
+function resetGame() {
+    let answer = document.getElementById('answer');
+    answer.value="";
+    let numberOne = Math.floor(Math.random() * 25) + 1;
+    let numberTwo = Math.floor(Math.random() * 25) + 1;
+    let firstValue = document.getElementById('first');
+    let secondValue = document.getElementById('second');
+    firstValue.innerHTML = numberOne;
+    secondValue.innerHTML = numberTwo;  
+    
+}
