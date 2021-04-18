4ba79c1aa84ea7dcdaef0f6086d144c56746ed5e
aaronh1986 <aaronharbourne@gmail.com>
2021-04-18 23:35:00 +0000

    Make game more up and running than previously it was

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,22 +1,20 @@
-function gameRules() {
+function calculateAnswer() {
     let numberOne = Math.floor(Math.random() * 25) + 1;
     let numberTwo = Math.floor(Math.random() * 25) + 1;
     let firstValue = document.getElementById('first');
     let secondValue = document.getElementById('second');
     firstValue.innerHTML = numberOne;
     secondValue.innerHTML = numberTwo;  
-}
-
-gameRules();
-
-function calculateAnswer() {
+    let submit = document.getElementById('submit');
     let answer = document.getElementById('answer');
     answer.focus();
-    answer.addEventListener('click', function() {
-if (answer.value === numberOne + numberTwo) {
+    submit.addEventListener('click', function() {
+if (answer.value == numberOne + numberTwo) {
     alert('I guess you\'re not a fucking idiot after all.')
+    
 } else {
     alert('NOOOOOOOOOOOOOO, WROOOOOOOOOOOOOOOONG')
+   
 }
 })
 }
