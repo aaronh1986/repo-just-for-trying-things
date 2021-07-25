57a663dc1c67a779ac379a1d9a2a7b764110107a
aaronh1986 <aaronharbourne@gmail.com>
2021-07-25 12:38:02 +0000

    Set up clock

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,45 +1,38 @@
 function showTime() {
-// Get current hours, mins and seconds from the new Date() method
-const date = new Date()
-var currentHour = date.getHours();
-var currentMin = date.getMinutes();
-var currentSec = date.getSeconds();
-var session = "AM"
+    currentTime = new Date()
+    h = currentTime.getHours();
+    m = currentTime.getMinutes();
+    s = currentTime.getSeconds();
+    message = ""
 
-// Add a 0 to minutes and seconds so that if under 10 to have two digits
-currentHour = (currentHour < 10) ? "0" + currentHour : currentHour;
-currentMin = (currentMin < 10) ? "0" + currentMin : currentMin;
-currentSec = (currentSec < 10) ? "0" + currentSec : currentSec;
+    h = (h < 10) ? "0" + h:h;
+    m = (m < 10) ? "0" + m:m;
+    s = (s < 10) ? "0" + s:s;
+ 
+    if (h > 0) {
+        message = "On seas with goal of day"
+    }
 
-// Create a 12-hour clock
-if(currentHour == 0) {
-    currentHour = 12;
-}
+    if(h > 5) {
+        message = "Day break and toil on the path"
+    }
 
-if (currentHour > 12) {
-    currentHour = currentHour - 12;
-    session = "PM"
-}
+    if(h > 12) {
+        message = "Good Afternoon, a break and back to the field"
+    }
 
-// Show time in hour:mins:sec format and insert into the DOM
-const currentTime = currentHour + ":" + currentMin + ":" + currentSec + " " + session;
-const time = document.querySelector('[data-time]');
-time.innerText = currentTime;
+    if(h > 18) {
+        message = "Land search for night shore"
+    }
 
-// Make the function repeat every second so as to function as an accurate clock
-setTimeout(showTime, 1000);
+    if(h > 21) {
+        message = "Go with the flow"
+    }
+
+    time = h + ":" + m + ":" + s + " " + message;
+
+    document.getElementById("clock").innerText = time;
+    setInterval(showTime, 1000);
 }
 
 showTime();
-
-// Add random numbers for addition of two numbers
-const randomNumber = Math.round(Math.random() * 100) + 1;
-
-let operand1 = document.getElementsByClassName('operand1')[0];
-let operator = document.getElementsByClassName('operator')[0];
-let operand2 = document.getElementsByClassName('operand2')[0];
-let answer = document.getElementsByClassName('answer')[0];
-operand1.innerText = randomNumber;
-operator.innerText = 'X';
-operand2.innerText = randomNumber;
-answer.innerText = operand1.value + operand2.value
