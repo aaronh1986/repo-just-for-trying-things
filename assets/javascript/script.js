53248d6bc60d0320c8a47460f092ea9ecaffcc5c
aaronh1986 <aaronharbourne@gmail.com>
2021-08-25 22:23:25 +0000

    Array.filter() try

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,32 +1,6 @@
-/* let card = document.getElementById('card');
-card.addEventListener('click', () => {
-    card.classList.remove('blue');
-    card.classList.add('orange');
-}) */
-
-/*let cards = Array.from(document.getElementsByClassName('card'));
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-        if(card.classList.contains('blue')){
-          card.classList.remove('blue');
-          card.classList.add('orange');
-        }else if(card.classList.contains('orange')) {
-          card.classList.remove('orange');
-          card.classList.add('blue');
-        }
-    })
-}) */
-
-let cards = Array.from(document.getElementsByClassName('card'));
-let images = Array.from(document.getElementsByClassName('band'));
-
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-       if(alt="Buzz") {
-            src = "assets/images/melvins.jpg"
-       }
-    })
-})
-
-
-
+var test = ["Hello", 0, false
+, undefined, null, "to", "", "YOU", "", "YO YO"];
+console.log(test.filter(fuck=>fuck!=""));
+console.log(test.filter(Boolean));
+console.log(test);
+console.log(test.filter(fuck=>fuck));
