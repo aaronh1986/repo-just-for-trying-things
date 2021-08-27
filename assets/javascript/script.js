cd62ec80e24026c4e298ed9caec7a7304e64d0dd
aaronh1986 <aaronharbourne@gmail.com>
2021-08-27 22:53:18 +0000

    Got image to turn on click and toggle on next click

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,3 +4,16 @@ console.log(test.filter(fuck=>fuck!=""));
 console.log(test.filter(Boolean));
 console.log(test);
 console.log(test.filter(fuck=>fuck));
+
+let buzz = document.getElementById('buzz');
+let dale = document.getElementById('dale');
+let trevor = document.getElementById('trevor');
+
+function bollocks() {
+    if (buzz.getAttribute('src') == "assets/images/buzz.jpeg") {
+        buzz.src = "assets/images/melvins.jpg";
+    } else  {
+        buzz.src = "assets/images/buzz.jpeg";
+    }
+}
+
